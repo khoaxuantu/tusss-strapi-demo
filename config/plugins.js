dd22b7a51f93035165372fa5d8bc22879db7a350
khoaxuantu <ngxktuzkai2000@gmail.com>
2024-07-10 01:46:11 +0700

    chore: public storage & disallow search engines

--- config/plugins.js
+++ config/plugins.js
@@ -4,7 +4,6 @@ module.exports = () => ({
       provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
         providerOptions: {
             bucketName: 'tusss-test-strapi',
-            publicFiles: false,
             uniform: false,
             basePath: '',
         },
