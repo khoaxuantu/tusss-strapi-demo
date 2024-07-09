e931c9c726bb61a1d23fd519b4af096ba09b7731
khoaxuantu <ngxktuzkai2000@gmail.com>
2024-07-09 23:55:56 +0700

    feat: add google cloud storage

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,13 @@
-module.exports = () => ({});
+module.exports = () => ({
+  upload: {
+    config: {
+      provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
+        providerOptions: {
+            bucketName: 'tusss-test-strapi',
+            publicFiles: false,
+            uniform: false,
+            basePath: '',
+        },
+    },
+  },
+});
